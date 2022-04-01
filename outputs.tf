6b2ec8b6a8e263cfd20260955fa8295c349a0e47
javierlga <javierlgac@gmail.com>
2022-04-01 12:40:14 -0700

    More outputs

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,10 @@ output "cluster_endpoint" {
   value = google_container_cluster.cluster.endpoint
 }
 
+output "cluster_certificate" {
+  value = base64decode(google_container_cluster.cluster.master_auth.0.cluster_ca_certificate)
+}
+
 output "cluster_client_certificate" {
-  value = google_container_cluster.cluster.master_auth.0.client_certificate
+  value = base64decode(google_container_cluster.cluster.master_auth.0.client_certificate)
 }
